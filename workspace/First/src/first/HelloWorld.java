eae4237a61116c2839c9f4a69dd59066ea6c7a9c
parkdonghwan97 <venus6941@gmail.com>
2022-05-19 01:01:51 +0900

    Java Start

---
+++ workspace/First/src/first/HelloWorld.java
@@ -0,0 +1,15 @@
+package first;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+//		System.out.println('hello');  # 자바는 출력할 떄 작은 따옴표가 안된다 ' '  
+		
+
+	}
+
+}
+
+
+
